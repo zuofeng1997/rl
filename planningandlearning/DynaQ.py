a16e845bcd3bbd31ecb716d10279216f4b9364bd
zuofeng <1065504865@qq.com>
2018-07-10 11:45:54 +0800

    add planningandlearning

---
+++ planningandlearning/DynaQ.py
@@ -0,0 +1,70 @@
+import numpy as np
+import gym
+import random
+env = gym.make("Taxi-v2")
+state_space = 500
+action_space = 6
+epsilon = 0.1
+q = np.zeros((state_space,action_space))
+num_episode = 30000
+model = {}
+n = 5
+
+def choose_action(state,epsilon):
+    if np.random.rand()>epsilon:
+        return np.argmax(q[state])
+    else:
+        return np.random.randint(action_space)
+
+
+def score(n_samples=100):
+    rewards = []
+    for _ in range(n_samples):
+        observation = env.reset()
+        cum_rewards = 0
+        while True:
+            action = choose_action(observation,0)
+            observation, reward, done, _ = env.step(action)
+            cum_rewards += reward
+            if done:
+                rewards.append(cum_rewards)
+                break
+    return np.mean(rewards)
+
+
+for i_episode in range(1,num_episode):
+    s = env.reset()
+    # if i_episode<0.8*num_episode:
+    #     epsilon = 1-0.9*(i_episode/(0.8*num_episode))
+    # else:
+    #     epsilon = 0.1
+    while True:
+        a = choose_action(s,epsilon)
+        s_,r,done,_ = env.step(a)
+        model[(s,a)] = (r,s_,done)  #model learning
+        if done:
+            target = r
+        else:
+            target = r+np.max(q[s_])
+        error = target-q[s][a]
+        q[s][a] += 0.01*error
+        for i in range(n):  #planning
+            (state,action) = random.choice(list(model.keys()))
+
+            (reward,next_state,is_end) = model[(state,action)]
+            if is_end:
+                target_plan = reward
+            else:
+                target_plan = reward+np.max(q[next_state])
+            error_plan = target_plan-q[state][action]
+            q[state][action] += 0.01*error_plan
+
+        if done:
+            break
+        s = s_
+
+    if i_episode%100==0:
+        mean_reward = score()
+        print("i_episode:",i_episode,"mean_reward:",mean_reward,"epsilon:",epsilon)
+
+
